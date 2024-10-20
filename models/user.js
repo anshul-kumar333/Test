76b092000eccaad6c73a43d81c8d5ffb42d0c2ee
anshul-kumar333 <opo160591@gmail.com>
2024-10-20 18:23:08 +0530

    Add files via upload

--- models/user.js
+++ models/user.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  fatherName: { type: String, required: true }, // Add father's name
+  batchTiming: { type: String, required: true }, // Add batch timing
 });
 
 module.exports = mongoose.model('User', userSchema);
